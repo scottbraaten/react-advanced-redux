41c880306312f32e15311a9d2cb086576ae5a564
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-05-29 22:32:58 -0500

    include useeffect fetch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -6,6 +7,15 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-ec3e5-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart ? <Cart /> : null}
